11d485f826788e9379e2c3454cd598f18650789e
PengJoy1106 <110644319@qq.com>
2022-07-20 22:18:51 +0800

    Site updated: 2022-07-20 22:18:51

---
+++ js/weather.js
@@ -0,0 +1,23 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "1",
+        "tmpColor": "FFFFFF",
+        "tmpSize": "16",
+        "cityColor": "FFFFFF",
+        "citySize": "16",
+        "aqiColor": "FFFFFF",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "top",
+        "horizontal": "left",
+        "left": "200",
+        "top": "10",
+        "key": "fc8c8326a7d8467ca48a28e41ff69ad3"
+    }
+}
